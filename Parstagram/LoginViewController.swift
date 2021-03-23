5e431a8812fb0c74f8b4acae391d28dbc341b7bd
csudh <csudh@ITL-MBP56255.local>
2021-03-23 10:16:32 -0700

    Add Login functionality
    Edits:
    - LoginViewController.swift onSignUp function
    - Edit main project file

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -6,16 +6,55 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var userNameField: UITextField!
+    
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        
+        
+        
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        
+        let user = PFUser()
+        user.username = userNameField.text
+        user.password = userNameField.text
+        
+        user.signUpInBackground{ (success, error) in
+            if error != nil
+            {
+                
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                
+            }
+            else
+            {
+                
+                print("Error: \(error?.localizedDescription)")
+                
+            }
+        }
+        
+        
+        
+    }
+    
     /*
     // MARK: - Navigation
 
