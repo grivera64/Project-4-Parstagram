5b2183930e06abf3ec55d7130b1ba7d31d7dd6ad
csudh <csudh@ITL-MBP56255.local>
2021-03-23 10:15:53 -0700

    Edit AppDelegate.swift
    Fix for Parse Database on Back4Apps.com

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        // --- Copy this only
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "0niDCKcucWZJXKmkn78A9uj96jk3pIjYuC0uHEw4" // <- UPDATE
+                $0.clientKey = "OzD9ySAMs2ZNKiFPBqqEjzYbduCToNRFbyBmyomD" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com/"
+        }
+        Parse.initialize(with: parseConfig)
+        
+        // --- end copy
+
+
         return true
     }
 
