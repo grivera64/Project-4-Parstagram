5a5abcd4c9d1b76bc377ebaea99c785139d3b16c
csudh <csudh@ITL-MBP56255.local>
2021-03-31 14:31:01 -0700

    Add comments to API

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -21,6 +21,8 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     var posts = [PFObject]()
     
+    var selectedPost: PFObject!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -89,6 +91,33 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
         //create the comment
         
+        let comment = PFObject(className: "Comments")
+        
+        comment["text"] = text
+
+        comment["posts"] = selectedPost
+
+        comment["author"] = PFUser.current()!
+
+        selectedPost.add(comment, forKey: "comments")
+
+        selectedPost.saveInBackground { (success, error) in
+            if success
+            {
+
+                print("Comment saved")
+
+            }
+            else
+            {
+
+                print("Error saving comment")
+
+            }
+        }
+        
+        tableView.reloadData()
+    
         //clear and dismiss the input bar
         commentBar.inputTextView.text = nil
         showsCommentBar = false
@@ -176,30 +205,11 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
             becomeFirstResponder()
             commentBar.inputTextView.becomeFirstResponder()
             
+            selectedPost = post
+            
         }
         
-    //        comment["text"] = "This is a random comment"
-    //
-    //        comment["posts"] = post
-    //
-    //        comment["author"] = PFUser.current()!
-    //
-    //        post.add(comment, forKey: "comments")
-    //
-    //        post.saveInBackground { (success, error) in
-    //            if success
-    //            {
-    //
-    //                print("Comment saved")
-    //
-    //            }
-    //            else
-    //            {
-    //
-    //                print("Error saving comment")
-    //
-    //            }
-    //    }
+  
         
     }
 
