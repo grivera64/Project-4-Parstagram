87e26cd4a720f6466c74c1c61dbca0a589c155c8
csudh <csudh@ITL-MBP56255.local>
2021-03-31 17:34:21 -0700

    Fixed Auto-Login feature
    - Properly instatiated the correct View Controller

--- Parstagram/SceneDelegate.swift
+++ Parstagram/SceneDelegate.swift
@@ -20,13 +20,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let _ = (scene as? UIWindowScene) else { return }
         
         
-        if (PFUser.current != nil)
+        if (PFUser.current() != nil)
         {
             
             let main = UIStoryboard(name: "Main", bundle: nil)
             
-            let feedNavigationController = main.instantiateViewController(identifier: "FeedNavigationViewController")
-            //window?.rootViewController = feedNavigationController
+            let feedNavigationController = main.instantiateViewController(identifier: "FeedNavigationController")
+            window?.rootViewController = feedNavigationController
             
         }
         
